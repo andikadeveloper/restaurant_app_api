8bf3556873e217045d9be62aae00c9a8fc6a7835
Muhammad Andika <andikarastafara93@gmail.com>
2021-10-21 18:32:07 +0700

    feat: add restaurant image placeholder

--- lib/app/modules/detail/views/widgets/image_with_icon_button.dart
+++ lib/app/modules/detail/views/widgets/image_with_icon_button.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restaurant_app_api/app/utils/constants.dart';
@@ -11,18 +12,27 @@ class ImageWithIconButton extends StatelessWidget {
     required this.favoriteButton,
   }) : super(key: key);
 
-  final String imageId;
+  final String? imageId;
   final Widget favoriteButton;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        ImageFiltered(
-          imageFilter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 0.8),
-          child: Image.network(
-            'https://restaurant-api.dicoding.dev/images/large/' + imageId,
-            fit: BoxFit.cover,
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          width: MediaQuery.of(context).size.width,
+          child: ImageFiltered(
+            imageFilter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 0.8),
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              placeholder: (context, ulr) => Center(
+                  child: CircularProgressIndicator(
+                color: primaryColor,
+              )),
+              imageUrl: 'https://restaurant-api.dicoding.dev/images/large/' +
+                  imageId!,
+            ),
           ),
         ),
         SafeArea(
